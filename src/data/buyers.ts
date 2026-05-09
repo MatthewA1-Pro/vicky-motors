9e243d1ad1e57a717f2f723c139d4e06512a77e2
MatthewA1-Pro <oderinwalematthew3@gmail.com>
2026-05-09 01:04:48 +0100

    docs: add recent buyers transaction history data

---
+++ src/data/buyers.ts
@@ -0,0 +1,40 @@
+export interface BuyerRecord {
+  id: number;
+  name: string;
+  vehicle: string;
+  amount: string;
+  location: string;
+}
+
+export const recentBuyers: BuyerRecord[] = [
+  { id: 1, name: "Prince Adebayo O.", vehicle: "Bugatti Chiron Pur Sport", amount: "$3,850,000", location: "Lagos, NG" },
+  { id: 2, name: "Dr. Elena Volkov", vehicle: "Rolls-Royce Spectre", amount: "$450,000", location: "London, UK" },
+  { id: 3, name: "Marcus Thorne", vehicle: "Lamborghini Revuelto", amount: "$620,000", location: "New York, US" },
+  { id: 4, name: "Satoshi Nakamoto Jr.", vehicle: "Ferrari SF90 Stradale", amount: "$550,000", location: "Tokyo, JP" },
+  { id: 5, name: "Isabella Rossi", vehicle: "Aston Martin Valhalla", amount: "$850,000", location: "Milan, IT" },
+  { id: 6, name: "Sheikh Hamdan A.", vehicle: "Pagani Huayra BC", amount: "$2,600,000", location: "Dubai, UAE" },
+  { id: 7, name: "Julian Chen", vehicle: "Porsche 911 GT3 RS", amount: "$310,000", location: "Singapore, SG" },
+  { id: 8, name: "Amara Okafor", vehicle: "Mercedes-Maybach S680", amount: "$240,000", location: "Abuja, NG" },
+  { id: 9, name: "Xavier Dupont", vehicle: "Bugatti Mistral", amount: "$5,000,000", location: "Monaco, MC" },
+  { id: 10, name: "Sophia Lorenza", vehicle: "Ferrari Daytona SP3", amount: "$2,250,000", location: "Geneva, CH" },
+  { id: 11, name: "David Beckham III", vehicle: "Bentley Batur", amount: "$2,000,000", location: "Los Angeles, US" },
+  { id: 12, name: "Hassan Al-Fayed", vehicle: "Rolls-Royce Boat Tail", amount: "$28,000,000", location: "Doha, QA" },
+  { id: 13, name: "Chioma Adeleke", vehicle: "Lamborghini Urus Performante", amount: "$280,000", location: "Lagos, NG" },
+  { id: 14, name: "Robert Sterling", vehicle: "McLaren Speedtail", amount: "$2,300,000", location: "Greenwich, US" },
+  { id: 15, name: "Lucia Fernandez", vehicle: "Koenigsegg Jesko", amount: "$3,000,000", location: "Madrid, ES" },
+  { id: 16, name: "Oliver Wright", vehicle: "Range Rover SV Carmel Ed.", amount: "$345,000", location: "Aspen, US" },
+  { id: 17, name: "Fatima Al-Sayed", vehicle: "Ferrari Purosangue", amount: "$420,000", location: "Riyadh, SA" },
+  { id: 18, name: "Kenji Tanaka", vehicle: "Lexus LFA Nürburgring", amount: "$1,100,000", location: "Osaka, JP" },
+  { id: 19, name: "Victoria Sterling", vehicle: "Porsche 918 Spyder", amount: "$1,800,000", location: "Miami, US" },
+  { id: 20, name: "Oluwaseun T.", vehicle: "Rolls-Royce Cullinan BB", amount: "$480,000", location: "Lekki, NG" },
+  { id: 21, name: "Maximilian Schwarz", vehicle: "Mercedes-AMG ONE", amount: "$2,700,000", location: "Munich, DE" },
+  { id: 22, name: "Yara Ibrahim", vehicle: "Aston Martin Valkyrie", amount: "$3,200,000", location: "Abu Dhabi, UAE" },
+  { id: 23, name: "Thomas Shelby IV", vehicle: "Bentley Continental GT Mulliner", amount: "$360,000", location: "Birmingham, UK" },
+  { id: 24, name: "Grace Wu", vehicle: "Lamborghini Aventador SVJ", amount: "$580,000", location: "Hong Kong, HK" },
+  { id: 25, name: "Alexander Romanov", vehicle: "Ferrari 812 Competizione", amount: "$650,000", location: "Moscow, RU" },
+  { id: 26, name: "Sarah Jenkins", vehicle: "Lucid Air Sapphire", amount: "$250,000", location: "Palo Alto, US" },
+  { id: 27, name: "Ibrahim Moussa", vehicle: "Rimac Nevera", amount: "$2,100,000", location: "Cairo, EG" },
+  { id: 28, name: "Liam O'Connor", vehicle: "Aston Martin DBS 770 Ultimate", amount: "$405,000", location: "Dublin, IE" },
+  { id: 29, name: "Chen Wei", vehicle: "Rolls-Royce Phantom VIII", amount: "$550,000", location: "Shanghai, CN" },
+  { id: 30, name: "Olamide J.", vehicle: "Lamborghini Huracán STO", amount: "$395,000", location: "Lagos, NG" },
+];
